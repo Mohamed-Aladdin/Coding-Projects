5e74b492a6983f1d64a2d9974a027b6700f16aee
Mohamed <moham3daladdin101@gmail.com>
2023-03-25 18:10:21 +0200

    Day 24: Also added Mail Merge Project Start

---
+++ 100 Days of Code/Mail Merge Project Start/main.py
@@ -0,0 +1,26 @@
+#TODO: Create a letter using starting_letter.txt 
+#for each name in invited_names.txt
+#Replace the [name] placeholder with the actual name.
+#Save the letters in the folder "ReadyToSend".
+    
+#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+def main():
+    guests = []
+
+    with open(r"Python\100 Days of Code\Mail Merge Project Start\Input\Names\invited_names.txt", "r") as names:
+        for name in names.readlines():
+            name = name.strip()
+            guests.append(name)
+
+    with open(r"Python\100 Days of Code\Mail Merge Project Start\Input\Letters\starting_letter.txt", "r") as template:
+        text = template.read()
+    
+    for guest in guests:
+        new_letter = text.replace("[name]", guest)
+        with open(fr"Python\100 Days of Code\Mail Merge Project Start\Output\ReadyToSend\{guest}_letter.txt", "w") as final_letter:
+            final_letter.write(new_letter)
+
+main()
