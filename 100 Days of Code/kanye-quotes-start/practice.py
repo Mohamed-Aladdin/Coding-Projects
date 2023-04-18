7e12268439befc897ecb396eb19bdae73c56f062
Mohamed <moham3daladdin101@gmail.com>
2023-04-18 17:22:05 +0200

    Day 33: Added mini practice API project using Kanye-Rest API

---
+++ 100 Days of Code/kanye-quotes-start/practice.py
@@ -0,0 +1,15 @@
+import requests
+
+url = "http://api.open-notify.org/iss-now.json"
+
+response = requests.get(url)
+response.raise_for_status()
+
+data = response.json()
+print(data)
+
+longitude = data["iss_position"]["longitude"]
+latitude = data["iss_position"]["latitude"]
+
+iss_coordinates = (longitude, latitude)
+print(iss_coordinates)
